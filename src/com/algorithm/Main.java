291835fd4aeb78fd36862297aafea2b298e83e11
Sasindu Alahakoon <47134066+SasinduDilshara@users.noreply.github.com>
2021-07-23 18:41:55 +0530

    first commit

---
+++ src/com/algorithm/Main.java
@@ -0,0 +1,15 @@
+package com.algorithm;
+
+import com.algorithm.Trees.BST;
+import com.algorithm.Trees.RedBlackBST;
+import com.algorithm.Trees.SplayBST;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        BST bst = new BST<5>();
+//        RedBlackBST redBlackBST;
+//        SplayBST splayBST;
+	    System.out.println("Hello");
+    }
+}
